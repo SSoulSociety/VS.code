794ed5c3dcb411c1930349d7e7c1b94ff4c5ce00
Mehmet <lalemehmet449@gmail.com>
2024-01-08 09:22:32 +0300

    .c

--- C/Switch_case/banka_otomasyonu.c
+++ C/Switch_case/banka_otomasyonu.c
@@ -9,10 +9,10 @@ do
 {
 
     printf("Lutfen bir islem seciniz:\n---------------------------- \n Para cekme:(1)\n Para yatirma:(2)\n Eft yapma:(3)\n Bakiye sorgula:(4)\n Kart iade:(5)\n----------------------------\n");
-    scanf("%d", islem);
+    scanf("%d", &islem);
     if(islem > 5)
     {
-    printf("Bilinmeyen işlem, lütfen 1-5 arasi bir sayi giriniz:");
+    printf("Bilinmeyen islem \nlutfen 1-5 arasi bir sayi giriniz:");
     }
    
 
@@ -21,10 +21,10 @@ do
 switch (islem)
 {
     case 1:
-        printf("Bakiyeniz: %d" , bakiye);
-        printf("Ne kadar para cekeceksiziz:\n ");
+        printf("Bakiyeniz: %d \nNe kadar para cekeceksiniz?", bakiye);
         scanf("%d" , tutar);
         bakiye -= tutar;
+        printf("Kalan bakiye: %d" , bakiye);
         break;
     case 2:
         printf("Girilecek tutari giriniz:\n ");
