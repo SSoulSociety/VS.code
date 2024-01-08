cd1bbccd487ac976ff5e1b4d3cd0999d460b587b
Mehmet <lalemehmet449@gmail.com>
2024-01-08 17:12:14 +0300

    .c

--- C/Functionss/add_func.c
+++ C/Functionss/add_func.c
@@ -7,11 +7,11 @@ int add(int num1, int num2) {
 
 int main(void)
 {
-    int num1,num2;
+    int a,b;
     printf("Pls enter 2 number: ");
-    scanf("%d %d" ,&num1 , &num2);
+    scanf("%d %d" ,&a , &b);
 
-    printf("Addition of these number: %d" , add(num1 , num2));
+    printf("Addition of these number: %d" , add(a , b));
 
     return 0;
 }
---
+++ C/Functionss/max_func.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int max(int num1, int num2, int num3)
+{
+    int max;
+
+    if(num1 > num2 && num1 > num3)
+    {
+        max = num1;
+    }
+    else if(num2 > num3 && num2 > num1)
+    {
+        max = num2;
+    }
+    else
+    {
+        max = num3;
+    }
+    return max;
+}
+
+
+int main(void){
+    int num1, num2, num3;
+    printf("Enter three number pls: \n");
+    scanf("%d %d %d" , &num1, &num2, &num3);    
+    printf("Max number is: %d\n" , max(num1, num2, num3));
+
+    return 0;
+}
