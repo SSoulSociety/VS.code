5309f2ceb0f959fcc75841fd994db27d3f15cedc
Mehmet <lalemehmet449@gmail.com>
2024-01-14 11:14:29 +0300

    .c

--- convert_binary_func.c
+++ convert_binary_func.c
@@ -8,29 +8,29 @@ int main(void)
     printf("Enter a number to convert binary: ");
     scanf("%d", &number);
 
-    void convertBin(int number);
+    convertBin(number);
 
     return 0;
 }
 void convertBin(int number) // function defintion
 {
     int original = number;
-    int x, sum = 1;
+    int remain, sum = 1;
     sum = (original % 2);
     original /= 2;
 
     while (original != 0)
     {
-       x = (original % 2);
-       if (x == 0)
-       {
-        sum = (sum * 10);
-       }
-       else
-       {    
-        sum = (sum * 10) + 1;
-       }
+        remain = (original % 2);
+        if (remain == 0)
+        {
+            sum = (sum * 10);
+        }
+        else
+        {
+            sum = (sum * 10) + 1;
+        }
         original /= 2;
     }
-    printf("%d" , sum);
+    printf("Binary representation: %d\n", sum);
 }
