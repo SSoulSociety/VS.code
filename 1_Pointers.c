abef08fe0443e260874346e029f72775686dd928
Mehmet LALE <lalemehmet449@gmail.com>
2024-03-07 17:55:55 +0300

    .c

---
+++ 1_Pointers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* We use pointers for:
+-pass by reference
+-pass functions between functions
+-manipualte strings and arrays
+-dynamic data structers */
+
+int main(void)
+{
+    int *countPtr, count; // * does not affect the second varaiables
+    // countPtr is a pointer to an integer
+    // '*' indicates that variable is a pointer
+    // Referencing a value through a pointer is called indirection or dereferenicng operator
+
+    int *examplePtr = NULL; // null means 0 or nothing
+
+    int y = 5;
+    int *yPtr = &y;
+    printf("lets print *yPtr = %d", *yPtr);
+
+    return 0;
+}
---
+++ 2_Pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 7;
+    int *aPtr = &a;
+    // %p prints adres as a hexadecimel integer
+    printf("Address of a is %p\nValue of aPtr is %p\n", &a, aPtr);
+    printf("\nValue of a is %d\nValue of *aPtr is %d\n\n", a, *aPtr);
+    printf("Showing that * and & are complements of each other\n");
+    printf("&*aPtr = %p\n*&aPtr = %p\n", &*aPtr, *&aPtr);
+    // aPtr coresspond to adress of a while *aPtre correspond to value of a
+    
+    return 0;
+}
