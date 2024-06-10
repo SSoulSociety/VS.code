e78eeaca19956c13f92b0aeef8293b8e45533d68
Mehmet LALE <lalemehmet449@gmail.com>
2024-06-11 00:49:14 +0300

    .c

--- Files/1_File_writing.c
+++ Files/1_File_writing.c
@@ -3,35 +3,27 @@
 
 int main(void)
 {
+    FILE *fPtr = NULL; // fPtr = clients.txt file pointer
 
-    FILE *fPtr = NULL; // fPtr = client.txt file pointer
-
-    // fopen opens the file. Exit the program if unable to create the file
-    if ((fPtr = fopen("clients.txt", "w")) == NULL)
-    {
-        puts("File could not be opened");
-    }
-    else
-    {
-        puts("Enter the acoount, name, and balance.");
-        puts("Enter EOF to end input");
-        printf("%s", "? ");
-    }
+    fPtr = fopen("clients.txt", "w");
+    
+    puts("Enter the account, name, and balance.");
+    puts("Enter EOF to end input");
+    printf("%s", "? ");
 
     int account = 0;      // account number
     char name[30] = "";   // account name
     double balance = 0.0; // account balance
-    scanf("%d %29s %lf", &account, name, &balance);
 
-    // write account, name and balance into file with fprintf
-    while (!feof(stdin))
-
+    while (scanf("%d %29s %lf", &account, name, &balance) == 3)
+    // If it reads and assigns all three values, it returns 3.
+    // If it fails to read three values it returns a value less than 3.
     {
-
+        // Write account, name and balance into file with fprintf
         fprintf(fPtr, "%d %s %.2f\n", account, name, balance);
         printf("%s", "? ");
-        scanf("%d%29s%lf", &account, name, &balance);
     }
 
     fclose(fPtr); // fclose closes file
+    return 0;     // Indicate successful completion
 }
